8cacdbbbae803dd49133f682e38c822b33532113
Czarina Cuarez <cuarezcs@students.nu-fairview.edu.ph>
2025-03-02 16:55:35 +0800

    Added translations and installed dependencies

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from 'react'
 import { MainRouter } from './main/routes/MainRouter'
-import LoadingSpinner from './component/LoadingSpinner/LoadingSpinner'
 
 function App() {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
     <MainRouter />
-  </Suspense>
   )
 }
 
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n.ts'
 import App from './App.tsx'
+import LoadingSpinner from './component/LoadingSpinner/LoadingSpinner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+   <Suspense fallback={<LoadingSpinner />}>
     <App />
+    </Suspense>
   </StrictMode>,
 )
